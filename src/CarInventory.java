f7862d90cd2b2f86d3ac4108d45dfce1457149c8
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 17:55:51 +0300

    Commit # 14                                              This loop iterates over each Car object in the cars list. For each car, it:-Calls the displaySpecifications() method to get and display the car details. Uses System.out.printf to format and display the import tax amount, using the getMake(), getModel(), and calculateImportTax() methods.

--- src/CarInventory.java
+++ src/CarInventory.java
@@ -1,4 +1,10 @@
 import java.util.List;
 public class CarInventory {
-    public static void displayCarInventory(List<Car> cars) {}
+    public static void displayCarInventory(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.displaySpecifications());
+            System.out.printf("Import Tax for %s %s: $%.2f%n", car.getMake(), car.getModel(), car.calculateImportTax());
+            System.out.println();
+        }
+    }
 }
