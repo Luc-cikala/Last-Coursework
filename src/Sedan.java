dab4aa38b9c03ca9b2ae3d2c13173b328735da7e
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 16:48:42 +0300

    Commit # 10                                               on line number 15 to LN 19 explain how This method overrides the abstract method calculateImportTax of the Car class. It calculates and returns the amount of import taxes based on a fixed rate (10% of the price).

--- src/Sedan.java
+++ src/Sedan.java
@@ -1,13 +1,20 @@
 public class Sedan extends Car {
     private int trunkSpace;
+
     public Sedan(String make, String model, int year, double price, double fuelConsumption, int trunkSpace) {
         super(make, model, year, price, fuelConsumption);
         this.trunkSpace = trunkSpace;
     }
+
     @Override
     public String displaySpecifications() {
         return "Sedan: " + make + " " + model + ", Year: " + year +
                 ", Price: $" + price + ", Trunk Space: " + trunkSpace +
                 " liters, Fuel Consumption: " + fuelConsumption + " L/100km";
+    }
+
+    @Override
+    public double calculateImportTax() {
+        return price * 0.1;
     }
 }
