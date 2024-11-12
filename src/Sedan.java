30de7793557d2728fbb0c6b99bbbf1a04be11d1d
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 14:23:53 +0300

    Commit #8                                                 on line number 3 up-to LN 6 we got a changes and The constructor initializes the attributes of the Sedan class. It calls the constructor of the parent class (Car) to initialize the inherited attributes and also initializes the trunkSpace attribute

--- src/Sedan.java
+++ src/Sedan.java
@@ -1,3 +1,7 @@
 public class Sedan extends Car {
     private int trunkSpace;
+    public Sedan(String make, String model, int year, double price, double fuelConsumption, int trunkSpace) {
+        super(make, model, year, price, fuelConsumption);
+        this.trunkSpace = trunkSpace;
+    }
 }
