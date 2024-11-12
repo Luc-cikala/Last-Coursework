3a6db6e21cd35ddb328a4d232252aaff83e09bf0
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 18:56:08 +0300

    Commit # 19                                                  These lines create instances of the Sedan, SUV, and Truck classes, passing the required parameters to the constructor of each class to initialize the attributes.                                                              - Create instances of Sedan, SUV, and Truck with their respective characteristics.                                                              - sedan1 : Toyota Camry (2022)                                                              - suv1 : Honda CR-V (2023)                                                              - truck1 : Ford F-150 (2021)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
+        Sedan sedan1 = new Sedan("Toyota", "Camry", 2022, 24000.00, 7.5, 500);
+        SUV suv1 = new SUV("Honda", "CR-V", 2023, 32000.00, 8.5, 5);
+        Truck truck1 = new Truck("Ford", "F-150", 2021, 35000.00, 12.0, 3.5);
+
     }
 }
