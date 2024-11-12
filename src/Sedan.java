3bc2024a4370d21c30851c2fb26fdbc4b54dada0
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 16:42:07 +0300

    Commit #9                                                on line number 7 to LN 12 This method overrides the abstract method displaySpecifications of the Car class. It returns a formatted string containing all specifications of the Sedan model.

--- src/Sedan.java
+++ src/Sedan.java
@@ -4,4 +4,10 @@ public class Sedan extends Car {
         super(make, model, year, price, fuelConsumption);
         this.trunkSpace = trunkSpace;
     }
+    @Override
+    public String displaySpecifications() {
+        return "Sedan: " + make + " " + model + ", Year: " + year +
+                ", Price: $" + price + ", Trunk Space: " + trunkSpace +
+                " liters, Fuel Consumption: " + fuelConsumption + " L/100km";
+    }
 }
