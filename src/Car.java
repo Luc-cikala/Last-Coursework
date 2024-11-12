83396bc7443c438d8ae034184d11d62b50bac3ce
Asifiwe cikala <112934976+asifiwe-cikala-luc@users.noreply.github.com>
2024-11-12 13:30:00 +0300

    Commit #3                                                   I have added a constructor on line number9

--- src/Car.java
+++ src/Car.java
@@ -4,4 +4,13 @@ public abstract class Car {
     protected int year;
     protected double price;
     protected double fuelConsumption;
+
+
+    public Car(String make, String model, int year, double price, double fuelConsumption) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.fuelConsumption = fuelConsumption;
+    }
 }
